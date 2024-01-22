798ebdedddc2d5ea65be40b7e6feab468b762c6e
HEEEUN9812 <heeeun9812@gmail.com>
2024-01-22 16:15:01 +0900

    2주차 숙제

--- src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -10,5 +10,6 @@ import java.util.List;
 //@Component // 해당 클래스를 bean으로 만듦
 public interface MemoRepository extends JpaRepository<Memo,Long> {
     List<Memo> findAllByOrderByModifiedAtDesc();
+    List<Memo> findAllByContentsContainsOrderByModifiedAtDesc(String keyword);
 //    List<Memo> findAllByUsername(String username);
 }
