604c3b9ea4f4cb690a85c3d1f25a5e3dd45b4cc0
HEEEUN9812 <heeeun9812@gmail.com>
2024-01-22 12:58:48 +0900

    Query Method란 무엇일까?

--- src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -4,8 +4,11 @@ import com.sparta.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 //@Component // 해당 클래스를 bean으로 만듦
 public interface MemoRepository extends JpaRepository<Memo,Long> {
-
+    List<Memo> findAllByOrderByModifiedAtDesc();
+//    List<Memo> findAllByUsername(String username);
 }
